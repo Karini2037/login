d51fdc4e70d3b142c8410aceea2d52c5fd55692c
Karini Sousa <131774110+Karini2037@users.noreply.github.com>
2025-04-23 19:23:20 -0300

    teste

--- flutter_login/lib/services/auth_service.dart
+++ flutter_login/lib/services/auth_service.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import "package:firebase_auth/firebase_auth.dart";
 import 'package:flutter/material.dart';
 
 /// Serviço de autenticação que gerencia todas as operações relacionadas ao Firebase Auth
